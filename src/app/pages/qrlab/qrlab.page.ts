3d6c66c6ab1b6842314e4eba79787b7eb12d1d55
danielcastillopalma <d.castillo3@duocuc.cl>
2025-04-21 17:44:43 -0400

    c

--- src/app/pages/qrlab/qrlab.page.ts
+++ src/app/pages/qrlab/qrlab.page.ts
@@ -1,41 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { IonContent, IonHeader, IonTitle, IonToolbar,IonButton } from '@ionic/angular/standalone';
 import { QRService } from 'src/app/services/qr.service';
-=======
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
->>>>>>> origin/main
 
 @Component({
   selector: 'app-qrlab',
   templateUrl: './qrlab.page.html',
   styleUrls: ['./qrlab.page.scss'],
   standalone: true,
-<<<<<<< HEAD
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
 })
 export class QrlabPage implements OnInit {
 
   constructor(public qr: QRService) { }
-=======
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
-})
-export class QrlabPage implements OnInit {
-
-  constructor() { }
->>>>>>> origin/main
 
   ngOnInit() {
   }
 
-<<<<<<< HEAD
   Scan() {
     this.qr.StartScan();
   }
 
-=======
->>>>>>> origin/main
 }
 
--- src/app/services/qr.service.ts
+++ src/app/services/qr.service.ts
@@ -58,9 +58,5 @@ export class QRService {
     document.querySelector('body')?.classList.remove('scanner-active');
     this.scan = false;
     this.scanResult = "Stop Scan";
-<<<<<<< HEAD
   }
 }
-=======
-  }
->>>>>>> origin/main
