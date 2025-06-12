607bfbbff87b6d0034eab07329deb12108d737e9
danielcastillopalma <d.castillo3@duocuc.cl>
2025-06-11 23:45:05 -0400

    ranking y recompensas

--- src/app/pages/rewards/rewards.page.ts
+++ src/app/pages/rewards/rewards.page.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonButton, IonLabel } from '@ionic/angular/standalone';
+import { ApirestService } from 'src/app/services/apirest.service';
 
 @Component({
   selector: 'app-rewards',
   templateUrl: './rewards.page.html',
   styleUrls: ['./rewards.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonButton, IonLabel]
 })
 export class RewardsPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private api: ApirestService) { }
+  rewards: { name: string; desc: number, ptj: number }[] = [];
   ngOnInit() {
+    this.getRewards();
+    console.log(this.rewards);
+  }
+
+  async getRewards() {
+    const response = await this.api.getRewards();
+
+    for (let reward of response) {
+      const [name, desc, ptj] = reward;
+      this.rewards.push(name, desc, ptj);
+    }
   }
 
 }
--- src/app/services/apirest.service.ts
+++ src/app/services/apirest.service.ts
@@ -69,6 +69,19 @@ export class ApirestService {
       return null;
     }
   }
+  public async getRewards() {
+    try {
+      const response = await fetch(`https://respawnen3.duckdns.org/api/rewards`);
+      if (!response.ok) {
+        throw new Error('Error en la respuesta del servidor al tratar de obtener los datos de recompensas');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
 
   public async postAporte(photoBase64: string, email, idpunto) {
     let base64 = photoBase64;
