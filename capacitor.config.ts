d1fc65f8719cdf6b2f006a73d9af548790548670
danielcastillopalma <d.castillo3@duocuc.cl>
2025-07-04 17:32:46 -0400

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.puntoverde.app',
+  appId: 'cl.puntoverdeduoc.app',
   appName: 'PuntoVerde',
   webDir: 'www',
   server: {
--- src/app/shared/constants/link-api.ts
+++ src/app/shared/constants/link-api.ts
@@ -1,1 +1,2 @@
-export const api = 'https://respawnen3.duckdns.org';
+//export const api = 'https://respawnen3.duckdns.org';
+export const api='https://13.220.85.246:3000';
